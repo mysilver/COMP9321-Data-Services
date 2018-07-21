5352cb77a705facb99c5131810794fa28877e604
mohammad <m.yaghubzade@gmail.com>
2018-07-21 14:52:52 +1000

    Week1-Activity1

--- Week2_DataAccess/activity_1.py
+++ Week2_DataAccess/activity_1.py
@@ -5,6 +5,10 @@ def read_csv(csv_file):
     return pd.read_csv(csv_file)
 
 
+def write_csv(dataframe, file):
+    dataframe.to_csv(file, sep=',', encoding='utf-8')
+
+
 def print_dataframe(df, print_colum=True, print_rows=True):
     # print column names
     if print_colum:
@@ -19,3 +23,4 @@ def print_dataframe(df, print_colum=True, print_rows=True):
 csv_file = '../datasets/Demographic_Statistics_By_Zip_Code.csv'
 dataframe = read_csv(csv_file)
 print_dataframe(dataframe)
+write_csv(dataframe, "../datasets/Demographic_Statistics.csv")
