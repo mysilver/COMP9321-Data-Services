5e22261352bdcb172e3bd9d3b16101e72e89dbbe
mohammad <m.yaghubzade@gmail.com>
2018-07-21 16:41:40 +1000

    Week2-Activity2

---
+++ Week2_DataAccess/activity_4.py
@@ -0,0 +1,46 @@
+import requests
+import pandas as pd
+from Week2_DataAccess.activity_1 import print_dataframe
+
+
+def get_json(url) -> dict:
+    """
+    :param url: RUL of the resouce
+    :return: JSON
+    """
+    resp = requests.get(url=url)
+    data = resp.json()
+    return data
+
+
+def json_to_dataframe(json_obj: dict) -> pd.DataFrame:
+    """
+    Please Open the JSON using the given URL to be familiar with the 
+    structure of the expected JSON object
+    
+    The root element contains two main elements : data and meta; 
+    the former contains the statistics for a given zip code, and 
+    the latter contains the information about the columns
+    :param json_obj: JSON object for the dataset
+    :return: A dataframe
+    """
+    # let's get the list of statistics for all zip codes
+    json_data = json_obj['data']
+
+    # to create a dataframe we also need the name of the columns:
+    columns = []
+    for c in json_obj['meta']['view']['columns']:
+        columns.append(c['name'])
+
+    return pd.DataFrame(data=json_data, columns=columns)
+
+
+if __name__ == '__main__':
+    url = "https://data.cityofnewyork.us/api/views/kku6-nxdu/rows.json"
+
+    print("Fetch the json")
+    json_obj = get_json(url)
+
+    print("Convert the json object to a dataframe")
+    df = json_to_dataframe(json_obj)
+    print_dataframe(df)
