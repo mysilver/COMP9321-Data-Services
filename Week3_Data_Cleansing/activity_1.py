0fcff8146706a406c3ce0784545a72396eb78606
MAY <m.yaghubzade@gmail.com>
2018-07-28 19:50:29 +1000

    Week3

--- Week3_Data_Cleansing/activity_1.py
+++ Week3_Data_Cleansing/activity_1.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 
 def print_dataframe(dataframe, print_column=True, print_rows=True):
@@ -15,14 +14,14 @@ def print_dataframe(dataframe, print_column=True, print_rows=True):
 
 if __name__ == "__main__":
     columns_to_drop = ['Edition Statement',
-                             'Corporate Author',
-                             'Corporate Contributors',
-                             'Former owner',
-                             'Engraver',
-                             'Contributors',
-                             'Issuance type',
-                             'Shelfmarks'
-                             ]
+                       'Corporate Author',
+                       'Corporate Contributors',
+                       'Former owner',
+                       'Engraver',
+                       'Contributors',
+                       'Issuance type',
+                       'Shelfmarks'
+                       ]
     csv_file = "Books.csv"
     df = pd.read_csv(csv_file)
 
@@ -30,7 +29,7 @@ if __name__ == "__main__":
     num_of_rows = df.shape[0]
     for column in df:
         percent = 100 * df[column].isnull().sum() / num_of_rows
-        print(column,  str(percent)+'%')
+        print(column, str(percent) + '%')
 
     print("****************************************")
     print("Dataframe before dropping the columns")
@@ -41,54 +40,3 @@ if __name__ == "__main__":
     df.drop(columns_to_drop, inplace=True, axis=1)
     print_dataframe(df)
     print("****************************************")
-
-
-#
-#
-# print(df['Identifier'].is_unique)
-# print(df['Title'].is_unique)
-# df.set_index('Identifier', inplace=True)
-# print("********************************")
-# print(df.loc[206])
-# # df = df.replace(r'^\s+', np.nan, regex=True)
-# # print("********************************")
-# # print(df.loc[206])
-# print("********************************")
-# # print(df.head())
-# dropped_df = df.drop(labels=['Edition Statement',
-#                              'Corporate Author',
-#                              'Corporate Contributors',
-#                              'Former owner',
-#                              'Engraver',
-#                              'Contributors',
-#                              'Issuance type',
-#                              'Shelfmarks'
-#                              ], inplace=True, axis=1)
-#
-#
-#
-#
-#
-#
-# # print(df.head())
-#
-#
-# # print (df.loc[206])
-#
-# new_date = df['Date of Publication'].str.extract(r'^(\d{4})', expand=False)
-# new_date = pd.to_numeric(new_date)
-# df['Date of Publication'] = new_date
-#
-# num_of_nulls = df['Date of Publication'].isnull().sum()
-#
-# # print(new_date.dtype)
-#
-#
-# london = df['Place of Publication'].str.contains('London')
-# oxford = df['Place of Publication'].str.contains('Oxford')
-#
-# df['Place of Publication'] = np.where(london, 'London',
-#                                       np.where(oxford, 'Oxford',
-#                                                df['Place of Publication'].str.replace('-', ' ')))
-#
-# print(df.head())
