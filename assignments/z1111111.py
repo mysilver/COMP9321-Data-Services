d1f6525f4ed608b3fb9560a18bbf99dda32b8398
mohammad <m.yaghubzade@gmail.com>
2019-10-09 16:45:53 +1100

    Week2-Activity2

---
+++ assignments/z1111111.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+
+def question_1():
+    print("--------------- question_1 ---------------")
+    pass
+
+
+def question_2():
+    print("--------------- question_2 ---------------")
+    pass
+
+
+def question_3():
+    print("--------------- question_3 ---------------")
+    pass
+
+
+def question_4():
+    print("--------------- question_4 ---------------")
+    pass
+
+
+def question_5():
+    print("--------------- question_5 ---------------")
+    pass
+
+
+def question_6():
+    print("--------------- question_6 ---------------")
+    pass
+
+
+def question_7():
+    print("--------------- question_7 ---------------")
+    pass
+
+
+def question_8():
+    print("--------------- question_8 ---------------")
+    pass
+
+
+if __name__ == "__main__":
+    question_1()
+    question_2()
+    question_3()
+    question_4()
+    question_5()
+    question_6()
+    question_7()
+    question_8()
