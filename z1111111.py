78e03db2401f480be373c0d97ba9098200193dc8
Mohammad-Ali Yaghoub-Zadeh-Fard <m.yaghubzade@gmail.com>
2021-03-02 08:54:13 +1100

    Update z1111111.py

--- z1111111.py
+++ z1111111.py
@@ -3,22 +3,30 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 import os
+import numpy as np
+import math
 
 studentid = os.path.basename(sys.modules[__name__].__file__)
 
+
 def log(question, output_df, other):
     print("--------------- {}----------------".format(question))
+
     if other is not None:
         print(question, other)
     if output_df is not None:
-        print(output_df.head(5).to_string())
+        df = output_df.head(5).copy(True)
+        for c in df.columns:
+            df[c] = df[c].apply(lambda a: a[:20] if isinstance(a, str) else a)
+
+        df.columns = [a[:10] + "..." for a in df.columns]
+        print(df.to_string())
 
 
-def question_1(exposure, countries, continents):
+def question_1(exposure, countries):
     """
     :param exposure: the path for the exposure.csv file
     :param countries: the path for the Countries.csv file
-    :param countries: the path for the Countries-Continents.csv file
     :return: df1
             Data Type: Dataframe
             Please read the assignment specs to know how to create the output dataframe
@@ -44,7 +52,7 @@ def question_2(df1):
     # Your code goes here ...
     #################################################
 
-    log("QUESTION 2", output_df=df2, other=(len(df2.columns), sorted(df2.columns)))
+    log("QUESTION 2", output_df=df2[["avg_latitude", "avg_longitude"]], other=df2.shape)
     return df2
 
 
@@ -60,13 +68,14 @@ def question_3(df2):
     # Your code goes here ...
     #################################################
 
-    log("QUESTION 3", output_df=df3, other=df3.index.name)
+    log("QUESTION 3", output_df=df3[['distance_to_Wuhan']], other=df3.shape)
     return df3
 
 
-def question_4(df3):
+def question_4(df2, continents):
     """
-    :param df3: the dataframe created in question 3
+    :param df2: the dataframe created in question 2
+    :param continents: the path for the Countries-Continents.csv file
     :return: df4
             Data Type: Dataframe
             Please read the assignment specs to know how to create the output dataframe
@@ -76,46 +85,44 @@ def question_4(df3):
     # Your code goes here ...
     #################################################
 
-    log("QUESTION 4", output_df=df4, other=(df4['budget'].min(), df4['budget'].max(), df4['budget'].mean()))
+    log("QUESTION 4", output_df=df4, other=df4.shape)
     return df4
 
 
-def question_5(df4):
+def question_5(df2):
     """
-    :param df4: the dataframe created in question 4
-    :return: df5
-            Data Type: Dataframe
+    :param df2: the dataframe created in question 2
+    :return: cities_lst
+            Data Type: list
             Please read the assignment specs to know how to create the output dataframe
     """
-
     #################################################
     # Your code goes here ...
     #################################################
 
-    log("QUESTION 5", output_df=df5,
-        other=(df5['success_impact'].min(), df5['success_impact'].max(), df5['success_impact'].mean()))
+    log("QUESTION 5", output_df=df5, other=df5.shape)
     return df5
 
 
-def question_6(df5):
+def question_6(df2):
     """
-    :param df5: the dataframe created in question 5
+    :param df2: the dataframe created in question 2
     :return: df6
             Data Type: Dataframe
             Please read the assignment specs to know how to create the output dataframe
     """
-
+    cities_lst = []
     #################################################
     # Your code goes here ...
     #################################################
 
-    log("QUESTION 6", output_df=df6, other=(df6['popularity'].min(), df6['popularity'].max(), df6['popularity'].mean()))
+    log("QUESTION 6", output_df=None, other=cities_lst)
     return df6
 
 
-def question_7(df6):
+def question_7(df2):
     """
-    :param df6: the dataframe created in question 6
+    :param df2: the dataframe created in question 2
     :return: df7
             Data Type: Dataframe
             Please read the assignment specs to know how to create the output dataframe
@@ -125,13 +132,14 @@ def question_7(df6):
     # Your code goes here ...
     #################################################
 
-    log("QUESTION 7", output_df=df7, other=df7['popularity'].dtype)
+    log("QUESTION 7", output_df=df7, other=df7.shape)
     return df7
 
 
-def question_8(df10):
+def question_8(df2, continents):
     """
-    :param df10: the dataframe created in question 10
+    :param df2: the dataframe created in question 2
+    :param continents: the path for the Countries-Continents.csv file
     :return: nothing, but saves the figure on the disk
     """
 
@@ -142,9 +150,9 @@ def question_8(df10):
     plt.savefig("{}-Q11.png".format(studentid))
 
 
-def question_9(df10):
+def question_9(df2):
     """
-    :param df10: the dataframe created in question 10
+    :param df2: the dataframe created in question 2
     :return: nothing, but saves the figure on the disk
     """
 
@@ -155,10 +163,11 @@ def question_9(df10):
     plt.savefig("{}-Q12.png".format(studentid))
 
 
-def question_10(df10):
+def question_10(df2, continents):
     """
-    :param df10: the dataframe created in question 10
+    :param df2: the dataframe created in question 2
     :return: nothing, but saves the figure on the disk
+    :param continents: the path for the Countries-Continents.csv file
     """
 
     #################################################
@@ -166,15 +175,16 @@ def question_10(df10):
     #################################################
 
     plt.savefig("{}-Q13.png".format(studentid))
-    
+
+
 if __name__ == "__main__":
-    df1 = question_1("exposure.csv", "Countries.csv", "Countries-Continents.csv")
-    df2 = question_2(df1)
-    df3 = question_3(df2)
-    df4 = question_4(df2)
-    df5 = question_5(df2)
-    lst = question_6(df2)
-    df7 = question_7(df2)
-    df8 = question_8(df2)
-    df9 = question_9(df2)
-    df10 = question_10(df2)
+    df1 = question_1("exposure.csv", "Countries.csv")
+    df2 = question_2(df1.copy(True))
+    df3 = question_3(df2.copy(True))
+    df4 = question_4(df2.copy(True), "Countries-Continents.csv")
+    df5 = question_5(df2.copy(True))
+    lst = question_6(df2.copy(True))
+    df7 = question_7(df2.copy(True))
+    question_8(df2.copy(True), "Countries-Continents.csv")
+    question_9(df2.copy(True))
+    question_10(df2.copy(True), "Countries-Continents.csv")
