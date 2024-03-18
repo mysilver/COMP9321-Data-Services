9d5725d4802ad20db221af6983c90e5d4561bfda
m.yaghubzade@gmail.com <m.yaghubzade@gmail.com>
2024-03-18 13:57:03 +1100

    T24 Updates

---
+++ 24T1/ass1/zXXXXXXX.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+COMP9321 24T1 Assignment 2
+Data publication as a RESTful service API
+
+Getting Started
+---------------
+
+1. You MUST rename this file according to your zID, e.g., z1234567.py.
+
+2. To ensure your submission can be marked correctly, you're strongly encouraged
+   to create a new virtual environment for this assignment.  Please see the
+   instructions in the assignment 1 specification to create and activate a
+   virtual environment.
+
+3. Once you have activated your virtual environment, you need to install the
+   following, required packages:
+
+   pip install python-dotenv==1.0.1
+   pip install google-generativeai==0.4.1
+
+   You may also use any of the packages we've used in the weekly labs.
+   The most likely ones you'll want to install are:
+
+   pip install flask==3.0.2
+   pip install flask_restx==1.3.0
+   pip install requests==2.31.0
+
+4. Create a file called `.env` in the same directory as this file.  This file
+   will contain the Google API key you generatea in the next step.
+
+5. Go to the following page, click on the link to "Get an API key", and follow
+   the instructions to generate an API key:
+
+   https://ai.google.dev/tutorials/python_quickstart
+
+6. Add the following line to your `.env` file, replacing `your-api-key` with
+   the API key you generated, and save the file:
+
+   GOOGLE_API_KEY=your-api-key
+
+7. You can now start implementing your solution. You are free to edit this file how you like, but keep it readable
+   such that a marker can read and understand your code if necessary for partial marks.
+
+Submission
+----------
+
+The only file you will submit is this Python file.
+
+You can submit your code using the following command when connected to CSE, and
+assuming your file is in the current directory (remember to replace `zid` with
+your actual zID, i.e. the name of this file after renaming it):
+
+give cs9321 assign2 zid.py requirements.txt
+
+You can also submit through WebCMS3, using the tab at the top of the assignment
+page.
+
+"""
+
+# You can import more modules from the standard library here if you need them
+# (which you will, e.g. sqlite3).
+import os
+from pathlib import Path
+
+# You can import more third-party packages here if you need them, provided
+# that they've been used in the weekly labs, or specified in this assignment,
+# and their versions match.
+from dotenv import load_dotenv          # needed to load the environment variables from the .env file
+import google.generativeai as genai     # needed to access the Generative AI API
+
+
+studentid = Path(__file__).stem         # Will capture your zID from the filename.
+db_file   = f"{studentid}.db"           # Use this variable when referencing the SQLite database file.
+pdf_file  = f"{studentid}.pdf"          # Use this variable when referencing the PDF file.
+
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+# Configure the API key
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+
+# Create a Gemini Pro model
+gemini = genai.GenerativeModel('gemini-pro')
+
+if __name__ == "__main__":
+    # Here's a quick example of using the Generative AI API:
+    question = "Give me some facts about UNSW!"
+    response = gemini.generate_content(question)
+    print(question, response.text)
